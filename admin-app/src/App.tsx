77e8bf383e7853b08df8b7a7eac554ea1653ed5e
rubenemailviralles-ops <rubenemailviralles@gmail.com>
2025-10-22 13:15:13 +0200

    Fix 404 on refresh - use HashRouter for GitHub Pages compatibility

--- admin-app/src/App.tsx
+++ admin-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import ContactMessages from './pages/ContactMessages'
@@ -105,6 +105,7 @@ function App() {
             <Route path="/archives" element={<Archives />} />
             <Route path="/install" element={<Install />} />
             <Route path="/notifications" element={<NotificationStatus />} />
+            <Route path="*" element={<Dashboard />} />
           </Routes>
         </Layout>
       </div>

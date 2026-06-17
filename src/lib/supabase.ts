f14903e3ba14c46c57d74b71bdec654760efc6c2
rubenemailviralles-ops <rubenemailviralles@gmail.com>
2026-06-17 12:40:22 +0200

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zqhidpuxseyiqeirdymu.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxaGlkcHV4c2V5aXFlaXJkeW11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NzI5MzEsImV4cCI6MjA5NzI0ODkzMX0.z6yiOyhh1RysF2B8nuxqgpNYIwaHzAg9ob8ti29sXBw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
